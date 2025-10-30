cbb25e429c202e0b53ede4eb05684e0ff693656c
MattGaviria <cmateogaviria@gmail.com>
2025-10-30 10:44:35 -0400

    Main activity logic created and xml updated

--- app/src/main/java/com/example/midterm_cesar_gaviriasepulveda/MainActivity.java
+++ app/src/main/java/com/example/midterm_cesar_gaviriasepulveda/MainActivity.java
@@ -7,18 +7,51 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TimesViewModel vm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        vm = new ViewModelProvider(this).get(TimesViewModel.class);
+
+        BottomNavigationView nav = findViewById(R.id.bottomNav);
+        nav.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.nav_table) {
+                showFragment(new TableFragment());
+                return true;
+            } else if (item.getItemId() == R.id.nav_history) {
+                showFragment(new HistoryFragment());
+                return true;
+            }
+            return false;
         });
+
+        if (savedInstanceState == null) {
+            nav.setSelectedItemId(R.id.nav_table);
+        }
+    }
+
+    public void switchToTable() {
+        BottomNavigationView nav = findViewById(R.id.bottomNav);
+        nav.setSelectedItemId(R.id.nav_table);
+    }
+
+    private void showFragment(Fragment f) {
+        FragmentManager fm = getSupportFragmentManager();
+        fm.beginTransaction()
+                .replace(R.id.fragmentContainer, f)
+                .commit();
     }
 }
